c7491fe0902f521c4f5c32f33b5d92bd0142255d
M T K POOJA <b24co041@mace.ac.in>
2026-03-27 07:43:53 +0530

    final change

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = 'YOUR_SUPABASE_URL'
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+
+export const supabase = createClient(https://mgegkbxspcynxeggjrlh.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZWdrYnhzcGN5bnhlZ2dqcmxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NDQwNjEsImV4cCI6MjA5MDEyMDA2MX0.WISm3ALQRmXt9bGfr7Mv_vIps1UmIjijAtya_ScRH_Y)
